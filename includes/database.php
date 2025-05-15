ad5a884042701d6c827f4f804ff47ee3939bff07
Mike Kirkwood-Smith <michael.kirkwood-smith3@tafensw.edu.au>
2025-05-15 21:54:33 +1000

    Changed database config include to check for local/remote environments and load connection details accordingly

--- includes/database.php
+++ includes/database.php
@@ -3,13 +3,28 @@
   // Dependencies
   require_once ROOT_DIR . "classes/DBAccess.php";
 
-  // TODO: Check if website is running locally (localhost, 127.0.0.1, ::1)
+  // Check if website is running locally (localhost, 127.0.0.1, ::1)
+  if (
+    $_SERVER["SERVER_NAME"] === "localhost" ||
+    $_SERVER["SERVER_ADDR"] === "127.0.0.1" ||
+    $_SERVER["SERVER_ADDR"] === "::1"
+  ) {
 
-  // Database config
-  $dbServer = "localhost";
-  $dbDatbase = "northwind";
-  $dbUsername = "root";
-  $dbPassword = "";
+    // Database config - local
+    $dbServer = "localhost";
+    $dbDatbase = "northwind";
+    $dbUsername = "root";
+    $dbPassword = "";
+
+  } else {
+
+    // Database config - remote
+    $dbServer = "localhost";
+    $dbDatbase = "navy31_northwind";
+    $dbUsername = "navy31_northwind";
+    $dbPassword = "c5dBiSl3?Kywx0#n";
+
+  }
 
   // Create a new DBAccess instance (this is used for ALL database operations!)
   $db = new DBAccess($dbServer, $dbDatbase, $dbUsername, $dbPassword);
