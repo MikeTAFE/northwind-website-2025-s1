7010890abfa7fe73acd32a6a6820586dfdb0dc14
Mike Kirkwood-Smith <michael.kirkwood-smith3@tafensw.edu.au>
2025-05-22 20:51:43 +1000

    Added staff.php page with listing of employees and images.

---
+++ staff.php
@@ -0,0 +1,32 @@
+<?php
+
+  // Common includes for main PHP pages (controllers)
+  require_once "includes/common.php";
+
+  // Config
+  $title = "Our Staff";
+
+  // Start output buffering (trap output - don't display it yet)
+  ob_start();
+
+  // Get all staff (employees)
+  $sql = <<<SQL
+    SELECT    EmployeeID, LastName, FirstName, Title, PhotoPath
+    FROM      employees
+    ORDER BY  LastName ASC
+  SQL;
+
+  // Prepare the statement
+  $stmt = $db->prepareStatement($sql);
+
+  // Execute query
+  $employees = $db->executeSQL($stmt);
+
+  // Include the page-specific template
+  include_once "templates/_staffPage.html.php";
+
+  // Stop output buffering - store output into the $content variable
+  $content = ob_get_clean();
+
+  // Include the main layout template
+  include_once "templates/_layout.html.php";
